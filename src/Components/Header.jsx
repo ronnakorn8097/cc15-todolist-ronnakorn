359c4a80b9a264498d09b1bb5473a378959ce1f1
ronnakorn8097 <ronnakorn.fahkiat@outlook.com>
2023-08-30 10:01:23 +0700

    edit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,8 @@
 import "./Header.scss";
 import { FaHome } from "react-icons/fa";
 import Search from "../Components/Search"; // import search สามารถตั้งชื่ออะไรก็ได้ แต่ใน tag jsx ต้องตั้งตาม แต่จะทำให้ ลำบากทำไมหวะ
+
+
 function Header() {
   return (
     <header className="header">
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,17 +1,16 @@
 
-import { FaSearch } from "react-icons/fa";
+import { FaSearch } from 'react-icons/fa';
+import './Search.scss';
 
-
-function Search ()
-{
+function Search() {
     return (
-        <div className="search">
-        <span className="search__icon">
-          <FaSearch/>
-        </span>
-        <input type="text" placeholder="search" className="search__input"/>
-      </div>
-    )
+        <div className='search'>
+            <span className='search__icon'>
+                <FaSearch />
+            </span>
+            <input type='text' placeholder='search' className='search__input' />
+        </div>
+    );
 }
 
 export default Search;
