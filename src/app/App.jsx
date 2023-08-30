3ac464995b721aa115ecc4386c18449236e3797e
ronnakorn8097 <ronnakorn.fahkiat@outlook.com>
2023-08-30 12:17:29 +0700

    update:sideBar

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,44 +1,39 @@
-import "./App.scss";
-// import { FaHome} from "react-icons/fa";
-// import { CiHome } from "react-icons/ci";
-import Header from "../Components/Header";
-import { FaInbox, FaCalendar, FaCalendarAlt, FaChevronDown } from 'react-icons/fa';
-
+// Dependencies
+import './App.scss';
+import { FaHome, FaInbox, FaCalendar, FaCalendarAlt } from 'react-icons/fa';
+import Header from '../Components/Header';
+import ListItem from '../Components/ListItem';
 
 function App() {
-  return (
-    <div className="todo">
-
-      <div className="todo__header">
-        <Header/>
-      </div>
-
-      <div className="todo__sidebar">
-     
-        <aside className="sidebar">
-          <section className="sidebar__category">
-          <ul className="list">
-              <li className="list__item">
-                <FaInbox className="list__item__icon" />
-                <p className="list__item__text">Inbox</p>
-              </li>
-              <li className="list__item">
-                <FaCalendar className="list__item__icon" />
-                <p className="list__item__text">Today</p>
-              </li>
-              <li className="list__item">
-                <FaCalendarAlt className="list__item__icon" />
-                <p className="list__item__text">Next 7 Days</p>
-              </li>
-            </ul>
-          </section>
-          <section className="sidebar__category">2</section>
-        </aside>
-      </div>
-     
-      <div className="todo__content">TodoContent</div>
-    </div>
-  );
+    return (
+        <div className='todo'>
+            <div className='todo__header'>
+                <Header />
+            </div>
+            <div className='todo__sidebar'>
+                <aside className='sidebar'>
+                    <section className='sidebar__category'>
+                        <ul className='list'>
+                            <ListItem
+                                text='Inbox'
+                                icon={<FaInbox className='list__item__icon' />}
+                            />
+                            <ListItem
+                                text='Today'
+                                icon={<FaCalendar className='list__item__icon' />}
+                            />
+                            <ListItem
+                                text='Next 7 Days'
+                                icon={<FaCalendarAlt className='list__item__icon' />}
+                            />
+                        </ul>
+                    </section>
+                    <section className='sidebar__category'>2</section>
+                </aside>
+            </div>
+            <div className='todo__content'>TodoContent</div>
+        </div>
+    );
 }
 
 export default App;
