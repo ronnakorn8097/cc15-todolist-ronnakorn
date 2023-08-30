fde6b4c2b23e3a955bc8adff1a7dda945f62ab7b
ronnakorn8097 <ronnakorn.fahkiat@outlook.com>
2023-08-30 14:22:40 +0700

    sideBar2 update

--- src/Components/ListItem.jsx
+++ src/Components/ListItem.jsx
@@ -1,14 +1,30 @@
 import "./ListItem.scss";
 import { FaInbox } from "react-icons/fa";
 
+/*
+props = {
+    text: string
+    icon: <Component />
+    active: boolean
+}
+
+
+*/
 function ListItem(props) {
-  console.log(props);
+  console.log(props); // props = {text: "custom text"}
+
+  const listClassName = `list__item ${props.active ? "active" : ""}`;
+  // active = false => textClassName = "list__item__text"
+  // active = true => textClassName = "list__item__text active"
   return (
-    <li className="list__item">
-        {props.icon}
-      <p className="list__item__text">{props.text}</p>
+    <li className={listClassName}>
+      {props.icon}
+      <p className="list_item_text">{props.text}</p>
     </li>
   );
 }
+// CSS + JS Value == Dynamics ClassName
+// not-active: className="list__item"
+// active: className="list__item active"
 
 export default ListItem;
--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,6 @@
 // Dependencies
 import './App.scss';
-import { FaHome, FaInbox, FaCalendar, FaCalendarAlt } from 'react-icons/fa';
+import { FaHome, FaInbox, FaCalendar, FaCalendarAlt,FaChevronDown } from 'react-icons/fa';
 import Header from '../Components/Header';
 import ListItem from '../Components/ListItem';
 
@@ -14,21 +14,45 @@ function App() {
                 <aside className='sidebar'>
                     <section className='sidebar__category'>
                         <ul className='list'>
+                          {/* Inboox */}
                             <ListItem
                                 text='Inbox'
-                                icon={<FaInbox className='list__item__icon' />}
+                                icon={<FaInbox/>}
+                                active={true}
                             />
+                            {/* Calender */}
                             <ListItem
                                 text='Today'
-                                icon={<FaCalendar className='list__item__icon' />}
+                                icon={<FaCalendar/>}
+                                active={false}
                             />
+                            {/* CalenderAlt */}
                             <ListItem
                                 text='Next 7 Days'
-                                icon={<FaCalendarAlt className='list__item__icon' />}
+                                icon={<FaCalendarAlt/>}
+                                active={false}
                             />
                         </ul>
                     </section>
-                    <section className='sidebar__category'>2</section>
+                    <section className='sidebar__category'>
+                    <div className='accordion'>
+                      {/* Toggle */}
+
+                        <div className='accordion__toggle'>
+                          <li className='accordion__item'>
+                            <FaChevronDown className='accordion__item__icon accordion__item__active'/>
+                            <p className='list__item__text'>Projects</p>
+                          </li>
+                        </div>
+
+                        {/* Lists */}
+                    <ul className="list">
+                      <ListItem text="Project-A" icon={<FaInbox />} active={true} />
+                      <ListItem text="Project-B" icon={<FaInbox />} active={false} />
+                    </ul>
+                    </div>
+
+                    </section>
                 </aside>
             </div>
             <div className='todo__content'>TodoContent</div>
